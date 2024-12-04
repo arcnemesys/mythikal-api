8133670c3c13bc1d7b160e3150d71f9c27e390aa
arcnemesys <accelerenzo@proton.me>
2024-12-04 18:01:28 -0500

    Working on deploying

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from typing import Union, Annotated, List
 from .auth import router as auth_router
 from db import engine 
 from sqlmodel import SQLModel
+from dotenv import load_dotenv
 
+load_dotenv()
 base_prompt = "Given what you know about me, my strengths and weaknesses, goals, fears, desires, accomplishments, hobbies, past, present, vices and virtues, can you generate a profile of me as an anime character, i.e powers, stats, abilities, biography, personality, fashion sense, etc? Feel free to draw on all categories of anime to do so, and, while remaining true to what you know about me, take creative liberty as you wish!"
 
 
